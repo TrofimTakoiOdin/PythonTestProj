2dd19757d1f9e5e54946bd9df683a7401b53551d
arseny <arseniy.trofimov1993@gmail.com>
2022-10-16 10:16:27 +0300

    Added list making

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+def chunked(lst, n):
+    k = []
+    for i in range(0, len(lst), n):
+        k.append(lst[i:i+n])
+    return k
 
+
+lst = input().split()
+n = int(input())
+print(chunked(lst, n))
